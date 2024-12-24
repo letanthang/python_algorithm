da3c738e7284b0c89065b4ea2c8c3416d811b4ab
Minton <letanthang@gmail.com>
2024-12-24 10:32:27 +0700

    draw tree

--- draw_tree.py
+++ draw_tree.py
@@ -7,40 +7,63 @@ def dfs(index):
         return
     if index != 0:
         parent = (index - 1) // 2
-        G.add_edge(parent, index)
-        update(G)
+        updateEdge(G, (parent, index))
 
     dfs(index * 2 + 1)
     dfs(index * 2 + 2)
 
 
-def update(G):
-    ax.clear()
+def build_graph(index):
+    if index > len(binary_tree):
+        return
+    if index != 0:
+        parent = (index - 1) // 2
+        G.add_edge(parent, index)
+    build_graph(index * 2 + 1)
+    build_graph(index * 2 + 2)
 
-    nx.draw(G, position, with_labels=True, node_color="lightblue", ax=ax)
+
+def updateEdge(G, edge):
+    nx.draw_networkx_edges(G, position, edgelist=[edge], edge_color="red", ax=ax)
     plt.draw()
     plt.pause(1)
 
 
-def preparePos(index, x, y, delta):
+def update(G):
+    ax.clear()
+
+    nx.draw(
+        G,
+        position,
+        with_labels=True,
+        node_color="lightblue",
+        ax=ax,
+    )
+    plt.draw()
+    plt.pause(1)
+
+
+def prepare_pos(index, x, y, delta):
     if index > len(binary_tree):
         return
 
     position[index] = (x, y)
 
-    preparePos(index * 2 + 1, x - delta, y - 1, delta / 2)
-    preparePos(index * 2 + 2, x + delta, y - 1, delta / 2)
+    prepare_pos(index * 2 + 1, x - delta, y - 1, delta / 2)
+    prepare_pos(index * 2 + 2, x + delta, y - 1, delta / 2)
 
 
-binary_tree = [3, 6, 7, 4, 9, 6, 11, 33]
+binary_tree = [3, 7, 4, 9, 6, 11, 33, 6]
 
 G = nx.DiGraph()
 plt.ion()
 fig, ax = plt.subplots()
 position = {}
-preparePos(0, 0.5, 0.98, 0.01)
+prepare_pos(0, 0.5, 0.98, 0.01)
 
-print(position)
+build_graph(0)
+update(G)
+
 dfs(0)
 
 plt.ioff()
